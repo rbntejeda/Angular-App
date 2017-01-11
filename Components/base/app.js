2b668648640d31cb451982f3890cdedff83914f1
Ruben <rbntejeda@hotmail.com>
2017-01-11 15:48:50 -0300

    base

---
+++ Components/base/app.js
@@ -0,0 +1,26 @@
+angular.module('app', ['components'])
+
+.controller('BeerCounter', function($scope, $locale) {
+  $scope.beers = [0, 1, 2, 3, 4, 5, 6];
+  if ($locale.id == 'en-us') {
+    $scope.beerForms = {
+      0: 'no beers',
+      one: '{} beer',
+      other: '{} beers'
+    };
+  } else {
+    $scope.beerForms = {
+      0: 'žiadne pivo',
+      one: '{} pivo',
+      few: '{} pivá',
+      other: '{} pív'
+    };
+  }
+});
+
+
+/*
+Copyright 2017 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
